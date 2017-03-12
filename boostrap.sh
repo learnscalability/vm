f443f8ca8a2a2fbc6d1d3efcec2541e403909fcb
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-03-12 17:52:47 +0000

    wip

--- boostrap.sh
+++ boostrap.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
 
-set -e # stop script if error occurs.
+set -e # Stop script if error occurs.
 
-# Install git
+# Install git.
 sudo apt-get update
 sudo apt-get install -y git
 
-# install go.
+# Install go.
 wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzvf go1.8.linux-amd64.tar.gz
-mkdir -p $HOME/go/src $HOME/go/bin $HOME/go/pkg
+mkdir -p $HOME/go/src/github.com $HOME/go/bin $HOME/go/pkg
 echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
 echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> $HOME/.bashrc
 echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
 echo "export PATH=$PATH:$HOME/go/bin:/usr/local/go/bin" >> $HOME/.bash_profile
 
-# Get hugo in order to build learnscalability.com
+# Link the learnscalability projects.
+ln -s /vagrant $HOME/go/src/github.com/learnscalability
+
+# Get hugo in order to build learnscalability.com.
 go get -u github.com/spf13/hugo
 
-# Get glide for package management
+# Get glide for package management.
 go get -u github.com/Masterminds/glide
+
+# Install protobuf
+wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
+sudo apt-get install -y unzip
+mkdir -p $HOME/proto
+unzip -u protoc-3.2.0-linux-x86_64.zip -d $HOME/proto
+echo "export PATH=$PATH:$HOME/proto/bin" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/proto/bin" >> $HOME/.bash_profile
+
+# Install gRPC and go bindings for protobuf
+go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+go get -u google.golang.org/grpc
